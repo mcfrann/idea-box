c7caa89db65ae8cc1f99fe441d9fe5dbf0f40ded
Madison Law <madisonlaw1@gmail.com>
2022-01-05 16:54:39 -0700

    Organize page & create global ideas variable

---
+++ main.js
@@ -0,0 +1,37 @@
+// Selector Variables here
+
+
+
+
+
+// query Save button in a variable
+// add event listener to button
+// function:
+    // declare variable for currentIdea and assign it to a new Idea instance
+    // If title.value or body.value is truthy, then push to ideas array
+    // 
+
+
+
+
+
+var ideas = [];
+
+
+// Event Lsiteners here
+
+
+
+
+
+
+
+
+// Event handlers here
+
+
+
+
+
+
+
