73f58b209416b460c2ba3d7de3b06364bffa1345
Kevin King <kevinturingking@gmail.com>
2022-01-05 16:30:03 -0700

    Build the data model

---
+++ Idea.js
@@ -0,0 +1,15 @@
+class Idea {
+  constructor(title,body){
+    this.title = title;
+    this.id = Date.now();
+    this.body = body;
+    this.star = false;
+  }
+  updateIdea() {
+    if (!this.star){
+      this.star = true
+    } else {
+      this.star = false
+    }
+  }
+}
