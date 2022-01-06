c8f80abf8c005a306f4cf3e7ff79f592af23faeb
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-06 16:41:06 -0700

    Refactor css and html

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ var saveButton = document.querySelector(".main-input-save");
 var ideaTitle = document.querySelector(".main-input-title");
 var ideaBody = document.querySelector(".main-input-body");
 var ideaContainer = document.querySelector(".idea-container");
-// var favoriteIcon = document.querySelector(".favorite-icon");
-// var favoriteIconActive = document.querySelector(".favorite-icon-active")
-
 
 
 // Global Variable
 
 var ideas = [];
-var currentIdea =
+var currentIdea;
 
 
 // Event Listeners here
@@ -23,42 +20,41 @@ ideaContainer.addEventListener('click', deleteIdea);
 ideaContainer.addEventListener('click', toggleIcon);
 
 
-
-
 // Event handlers here
 
 
 function loadIdeaGrid(e) {
-    e.preventDefault();
-    if (ideaTitle.value && ideaBody.value) {
-        saveIdea();
-        createIdeaCard();
-        clearFormInputs();
-        disableSaveButton()
-    }
+  e.preventDefault();
+  if (ideaTitle.value && ideaBody.value) {
+      saveIdea();
+      createIdeaCard();
+      clearFormInputs();
+      disableSaveButton();
+  }
 };
 
 function createIdeaCard () {
-    ideaContainer.innerHTML = "";
-    for (var i = 0; i < ideas.length; i ++) {
-        ideaContainer.innerHTML += `
-        <div class="idea-card-container" id=${ideas[i].id}>
-            <div class="idea-header">
-            <img class="favorite-icon" id=${ideas[i].id} src="assets/star.svg" alt="favorite-idea"/>
-            <img class="favorite-icon-active hidden" id=${ideas[i].id} src="assets/star-active.svg" alt="active-favorite-idea"/>
-            <img class="delete-icon" id=${ideas[i].id} src="assets/delete.svg" alt="delete-idea"/></div>
-            <div class="idea-body">
-                <h4>${ideas[i].title}</h4>
-                <p>${ideas[i].body}</p>
-            </div>
-            <div class="idea-footer"></div>
-        </div>`
-    }
+  ideaContainer.innerHTML = "";
+  for (var i = 0; i < ideas.length; i ++) {
+      ideaContainer.innerHTML += `
+      <div class="idea-card-container" id=${ideas[i].id}>
+          <div class="idea-header">
+          <img class="favorite-icon" id=${ideas[i].id} src="assets/star.svg" alt="favorite-idea"/>
+          <img class="favorite-icon-active hidden" id=${ideas[i].id} src="assets/star-active.svg" alt="active-favorite-idea"/>
+          <img class="delete-icon" id=${ideas[i].id} src="assets/delete.svg" alt="delete-idea"/>
+          </div>
+          <div class="idea-body">
+              <h4>${ideas[i].title}</h4>
+              <p>${ideas[i].body}</p>
+          </div>
+          <div class="idea-footer"></div>
+      </div>`
+  }
 };
 
 function saveIdea() {
-     currentIdea = new Idea(ideaTitle.value, ideaBody.value);
-    ideas.push(currentIdea)
+  currentIdea = new Idea(ideaTitle.value, ideaBody.value);
+  ideas.push(currentIdea);
 };
 
 function enableSaveButton() {
@@ -71,31 +67,29 @@ function disableSaveButton() {
 
 
 function clearFormInputs() {
-    ideaTitle.value = "";
-    ideaBody.value = ""
+  ideaTitle.value = "";
+  ideaBody.value = "";
 };
 
 function deleteIdea() {
   for (var i = 0; i < ideas.length; i++) {
-    if (ideas[i].id == event.target.closest(".delete-icon").id){
-      ideas.splice(i,1)
-  }
+    if (ideas[i].id == event.target.closest(".delete-icon").id) {
+      ideas.splice(i,1);
+    }
   }
   createIdeaCard();
-}
+};
 
-function toggleIcon(event) {
+function toggleIcon() {
   var favoriteIcon = document.querySelector(".favorite-icon");
   var favoriteIconActive = document.querySelector(".favorite-icon-active");
-
-  if(event.target.className === "favorite-icon" || event.target.className === "favorite-icon-active" ) {
+  if(event.target.className === "favorite-icon" || event.target.className === "favorite-icon-active") {
     favoriteIcon.classList.toggle("hidden");
     favoriteIconActive.classList.toggle("hidden");
   }
-  updateStar()
-}
+  updateStar();
+};
 
 function updateStar() {
-  currentIdea.updateIdea()
-  console.log(currentIdea)
-}
+  currentIdea.updateIdea();
+};
