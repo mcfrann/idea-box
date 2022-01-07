c93fc3d49eb86e231540eb56e9276322b1153f70
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-07 16:25:39 -0700

    Show favorites without array

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ var ideaContainer = document.querySelector(".idea-container");
 var showStarred = document.querySelector(".starred-btn");
 
 
+
+
 // Global Variable
 
 var ideas = [];
-var favoriteIdeas = [];
+// var favoriteIdeas = [];
 var currentIdea;
 
 
@@ -36,22 +38,22 @@ function loadIdeaGrid(e) {
   }
 };
 
-function createIdeaCard(ideaArray) {
+function createIdeaCard() {
   ideaContainer.innerHTML = "";
-  for (var i = 0; i < ideaArray.length; i ++) {
+  for (var i = 0; i < ideas.length; i ++) {
       ideaContainer.innerHTML += `
-      <div class="idea-card-container" id=${ideaArray[i].id}>
-          <div class="idea-header">
-          <img class="favorite-icon" id=${ideaArray[i].id} src="assets/star.svg" alt="favorite-idea"/>
-          <img class="favorite-icon-active hidden" id=${ideaArray[i].id} src="assets/star-active.svg" alt="active-favorite-idea"/>
-          <img class="delete-icon" id=${ideaArray[i].id} src="assets/delete.svg" alt="delete-idea"/>
-          </div>
-          <div class="idea-body">
-              <h4>${ideaArray[i].title}</h4>
-              <p>${ideaArray[i].body}</p>
-          </div>
-          <div class="idea-footer"></div>
-      </div>`
+    <div class="idea-card-container" id=${ideas[i].id}>
+        <div class="idea-header">
+        <img class="favorite-icon" id=${ideas[i].id} src="assets/star.svg" alt="favorite-idea"/>
+        <img class="favorite-icon-active hidden" id=${ideas[i].id} src="assets/star-active.svg" alt="active-favorite-idea"/>
+        <img class="delete-icon" id=${ideas[i].id} src="assets/delete.svg" alt="delete-idea"/>
+        </div>
+        <div class="idea-body">
+            <h4>${ideas[i].title}</h4>
+            <p>${ideas[i].body}</p>
+        </div>
+        <div class="idea-footer"></div>
+    </div>`
   }
 };
 
@@ -88,66 +90,74 @@ function toggleIcon() {
   var favoriteIconActive = document.querySelectorAll(".favorite-icon-active");
   for (var i = 0; i < ideas.length; i++) {
     if (ideas[i].id == event.target.id) {
-      addToFavorites(ideas[i]);
+      // addToFavorites(ideas[i]);
       favoriteIcon[i].classList.toggle("hidden");
       favoriteIconActive[i].classList.toggle("hidden");
+      updateStar(ideas[i]);
     }
   }
-
-  updateStar();
 };
 
-function updateStar() {
-  currentIdea.updateIdea();
+function updateStar(idea) {
+  idea.updateIdea();
+  // toggleIcon();
 };
 
-function addToFavorites(newFavIdea) {
-  favoriteIdeas.push(newFavIdea);
-}
+// function addToFavorites(newFavIdea) {
+//   favoriteIdeas.push(newFavIdea);
+// };
 
 function showFavorites() {
-  toggleFavIcon();
   if (showStarred.innerText === "Show Starred Ideas") {
     showStarred.innerText = "Show All Ideas";
-    createIdeaCard(favoriteIdeas);
+    renderFavoriteCards();
   } else {
     showStarred.innerText = "Show Starred Ideas";
-    createIdeaCard(ideas);
+    createIdeaCard();
   }
-}
+};
 
-function toggleFavIcon() {
-  var favoriteIcon = document.querySelectorAll(".favorite-icon");
-  var favoriteIconActive = document.querySelectorAll(".favorite-icon-active");
-  for (var i = 0; i < ideas.length; i++) {
+function renderFavoriteCards() {
+  ideaContainer.innerHTML = "";
+  for (var i = 0; i < ideas.length; i ++) {
     if (ideas[i].star) {
-      favoriteIcon[i].classList.add("hidden");
-      favoriteIconActive[i].classList.remove("hidden");
+      ideaContainer.innerHTML += `
+      <div class="idea-card-container" id=${ideas[i].id}>
+          <div class="idea-header">
+          <img class="favorite-icon hidden" id=${ideas[i].id} src="assets/star.svg" alt="favorite-idea"/>
+          <img class="favorite-icon-active" id=${ideas[i].id} src="assets/star-active.svg" alt="active-favorite-idea"/>
+          <img class="delete-icon" id=${ideas[i].id} src="assets/delete.svg" alt="delete-idea"/>
+          </div>
+          <div class="idea-body">
+              <h4>${ideas[i].title}</h4>
+              <p>${ideas[i].body}</p>
+          </div>
+          <div class="idea-footer"></div>
+      </div>`
     }
   }
-}
+};
 
+function showIcon(selectorVariable) {
+  selectorVariable.classList.remove("hidden");
+};
 
-  // ideaContainer.innerHTML = "";
-  // for (var i = 0; i < ideas.length; i ++) {
-  //   if (ideas[i].star) {
-  //     ideaContainer.innerHTML += `
-  //     <div class="idea-card-container" id=${ideas[i].id}>
-  //         <div class="idea-header">
-  //         <img class="favorite-icon" id=${ideas[i].id} src="assets/star.svg" alt="favorite-idea"/>
-  //         <img class="favorite-icon-active hidden" id=${ideas[i].id} src="assets/star-active.svg" alt="active-favorite-idea"/>
-  //         <img class="delete-icon" id=${ideas[i].id} src="assets/delete.svg" alt="delete-idea"/>
-  //         </div>
-  //         <div class="idea-body">
-  //             <h4>${ideas[i].title}</h4>
-  //             <p>${ideas[i].body}</p>
-  //         </div>
-  //         <div class="idea-footer"></div>
-  //     </div>`
-    // }
-  // }
+function hideIcon(selectorVariable) {
+  selectorVariable.classList.add("hidden");
+};
+
+// function checkStar() {
+//   for (var i = 0; i < ideas.length; i ++) {
+//     if (ideas[i].star)
+//   }
 // }
 
-// function createFavoritesView() {
-//
-// }
+
+// function toggleFavIcon() {
+//   var favoriteIcon = document.querySelectorAll(".favorite-icon");
+//   var favoriteIconActive = document.querySelectorAll(".favorite-icon-active");
+//   for (var i = 0; i < favoriteIdeas.length; i++) {
+//     favoriteIcon[i].classList.add("hidden");
+//     favoriteIconActive[i].classList.remove("hidden");
+//   }
+// };
